944c50fdc5a4f376406d262341121e2c9f5dfa96
Samirtchak <mtcha015@uottawa.ca>
2020-07-08 19:10:49 -0400

    Implement User Signup feature

--- app/index.js
+++ app/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const InitiateMongoConnection = require("./config/db");
+const signup = require("./routes/user"); //signup route
+
+
+// Initiate Mongo Server
+InitiateMongoConnection();
 
 const app = express();
 
 // PORT 
 const PORT = process.env.PORT || 4000;
 
+// Middleware
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
     res.json({ message: "API Working" });
   });
+
+app.use("/user", signup); // for signup feature
+
 
   app.listen(PORT, (req, res) => {
     console.log(`Server Started at PORT ${PORT}`);
