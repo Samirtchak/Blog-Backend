aac5959c849037c29b1bd8d5f98db7e41a58b16c
Samirtchak <mtcha015@uottawa.ca>
2020-07-09 14:15:31 -0400

    added missing username test, create existing user test, invalid password and email test

--- app/test/test.js
+++ app/test/test.js
@@ -29,6 +29,70 @@ it('test to create user should pass', (done) => {
         }) 
 
   })
+//test 2: missing username work as well with missing email and missing password
+it('username is missing', (done) => {
+        
+    chai.request(app)
+    .post('/user/signup')
+    .send({username:'', email:'fakeuser@sample.com', password:'1234SAMIR'})
+    .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res).to.be.json;
+        done()
+
+        }) 
+
+  })
+
+  //test 3: password is less than 6 character
+  it('password less than 6 character', (done) => {
+        
+    chai.request(app)
+    .post('/user/signup')
+    .send({username:'fakeuser', email:'fakeuser@sample.com', password:'sam'})
+    .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res).to.be.json;
+        done()
+
+        }) 
+
+  })
+
+// test 4: try to create an existing user
+it('create an existing user', (done) => {
+        
+    chai.request(app)
+    .post('/user/signup')
+    .send({username:'testuser', email:'testuser@example.com', password:'testpassword'})
+    .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res).to.be.json;
+        done()
+
+        }) 
+
+  })
+
+  // test 4: provide an invalid mail
+it('invalid mail test', (done) => {
+        
+    chai.request(app)
+    .post('/user/signup')
+    .send({username:'testuser', email:'anemail', password:'testpassword'})
+    .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res).to.be.json;
+        done()
+
+        }) 
+
+  })
+
 
 })
 
