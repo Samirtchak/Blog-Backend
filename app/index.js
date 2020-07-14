4e0bcb80a4632f2d904f9d6eb8dd13502efddad3
Samirtchak <mtcha015@uottawa.ca>
2020-07-13 22:11:59 -0400

    added update article feature

--- app/index.js
+++ app/index.js
@@ -5,8 +5,8 @@ const signup = require("./routes/user"); //signup route
 const login = require("./routes/login"); //login route
 const article = require("./routes/article"); //article route
 const get_all_article = require("./routes/getallarticle"); //get all articles route
-const get_article_by_id = require("./routes/getarticlebyId"); //get all articles route
-
+const get_article_by_id = require("./routes/getarticlebyId"); //get all article by id route
+const update_article = require("./routes/updateArticle"); // update an article route
 
 
 
@@ -30,6 +30,7 @@ app.use("/user", login); // for login feature
 app.use("/article", article); // for article feature
 app.use("/article", get_all_article); // for get article feature
 app.use("/article", get_article_by_id); // for get article by id feature
+app.use("/article", update_article); // for update an article feature
 
 
 
