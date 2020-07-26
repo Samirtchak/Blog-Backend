ca158f3f62af8b9ae90718d333a75f70d36bc242
Samirtchak <43187211+Samirtchak@users.noreply.github.com>
2020-07-26 14:01:37 -0400

    Use env variables to keep auth info secret

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-const URI = 'mongodb+srv://samir:quelafamille@sam-q8erk.mongodb.net/Blog?retryWrites=true&w=majority';
+const USERNAME = process.env.username;
+const PASSWORD = process.env.password;
+
+const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@sam-q8erk.mongodb.net/Blog?retryWrites=true&w=majority`;
 
 const InitiateMongoConnection = async () => {
     try {
